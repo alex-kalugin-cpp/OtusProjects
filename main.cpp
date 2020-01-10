9589fbdc9f48016253cb24e6e0541743e5aa7405
alex-kalugin-cpp <bubuzant@gmail.com>
2020-01-10 08:11:28 +0300

    2 PC working fine

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    //test from second PC from first PC
+    //test from second PC from first PC from second PC
     cout << "Hello World!" << endl;
     return 0;
 }
