ca2406d3dcbda2f67574b3fc5167ca0b2025f26e
alex-kalugin-cpp <bubuzant@gmail.com>
2020-01-10 05:24:40 +0300

    test from second PC from first PC

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    //test from second PC
-    cout << "Hello World!!!" << endl;
+    //test from second PC from first PC
+    cout << "Hello World!" << endl;
     return 0;
 }
