6f08ad14ac2a8ddf4232510ba6d067fdac913682
alex-kalugin-cpp <bubuzant@gmail.com>
2020-01-12 12:00:45 +0300

    add version.in.h

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include "version.h"
 using namespace std;
 
 int main()
 {
     //test from second PC from first PC from second PC
     cout << "Hello World!!" << endl;
+    //if()
     return 0;
 }
