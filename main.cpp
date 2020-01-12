c8d0b58bdc14a636b85cb9f1a60a3ddd56275da3
alex-kalugin-cpp <bubuzant@gmail.com>
2020-01-12 12:58:25 +0300

    + lib.cpp + lib.h + version

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#include "lib.h"
 #include <iostream>
-#include "version.h"
+//#include "version.h"
 using namespace std;
 
 int main()
 {
     //test from second PC from first PC from second PC
     cout << "Hello World!!" << endl;
+     std::cout << "Version: " << version() << std::endl;
+    //cout << PROJECT_VERSION_PATCH << endl;
     //if()
     return 0;
 }
