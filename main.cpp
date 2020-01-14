a68430417b55ed0ba3b4a616ccd25c91e1690a2c
alex-kalugin-cpp <bubuzant@gmail.com>
2020-01-14 20:11:33 +0300

    To cheking 1

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
     //test from second PC from first PC from second PC
-    cout << "Hello World!!" << endl;
-     std::cout << "Version: " << version() << std::endl;
+    cout << "Hello, World!" << endl;
+     //std::cout << "Version: " << version() << std::endl;
     //cout << PROJECT_VERSION_PATCH << endl;
     //if()
     return 0;
